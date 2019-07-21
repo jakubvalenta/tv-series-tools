5c4fe63d9207a70720a22acde8e1a0bd01494728
Jakub Valenta <jakub@jakubvalenta.cz>
2019-07-21 13:40:03 +0200

    search_subs: Don't remove existing matches

--- tv_series/search_subs.py
+++ tv_series/search_subs.py
@@ -1,10 +1,10 @@
+import itertools
 import os
 import re
 import sys
 
 import listio
 from termcolor import colored
-
 from tv_series import common
 
 
@@ -41,21 +41,28 @@ def search_subs(paths_and_subs, excl, regex_list):
         for sub in subs:
             match = common.convert_sub_to_match(file_path, sub)
             if match in excl:
-                print('ALREADY PROCESSED "{f}" {s} --> {e}'.format(
-                    f=match['file_path'],
-                    s=match['time_start'],
-                    e=match['time_end']))
+                print(
+                    'ALREADY PROCESSED "{f}" {s} --> {e}'.format(
+                        f=match['file_path'],
+                        s=match['time_start'],
+                        e=match['time_end'],
+                    )
+                )
                 continue
             line = common.parse_sub_text(sub.text)
             if not search_line(line, regex_list):
                 continue
             else:
-                print(colored(
-                    'MATCHED "{f}" {s} --> {e}'.format(
-                        f=match['file_path'],
-                        s=match['time_start'],
-                        e=match['time_end']),
-                    'green'))
+                print(
+                    colored(
+                        'MATCHED "{f}" {s} --> {e}'.format(
+                            f=match['file_path'],
+                            s=match['time_start'],
+                            e=match['time_end'],
+                        ),
+                        'green',
+                    )
+                )
                 yield match
 
 
@@ -72,13 +79,27 @@ def search_subs_and_save_matches():
     parser = argparse.ArgumentParser(
         description='TV Series Tools: Search subtitles'
     )
-    parser.add_argument('--input', '-i', dest='inputdir', required=True,
-                        help='path to a directory with the subtitle SRT files')
-    parser.add_argument('--output', '-o', dest='outputfile', required=True,
-                        help='path to a file in which the matches will be'
-                        ' stored')
-    parser.add_argument('--patterns', '-p', dest='patterns', required=True,
-                        help='path to a file with search patterns')
+    parser.add_argument(
+        '--input',
+        '-i',
+        dest='inputdir',
+        required=True,
+        help='path to a directory with the subtitle SRT files',
+    )
+    parser.add_argument(
+        '--output',
+        '-o',
+        dest='outputfile',
+        required=True,
+        help='path to a file in which the matches will be' ' stored',
+    )
+    parser.add_argument(
+        '--patterns',
+        '-p',
+        dest='patterns',
+        required=True,
+        help='path to a file with search patterns',
+    )
     args = parser.parse_args()
 
     if os.path.isfile(args.outputfile):
@@ -89,15 +110,14 @@ def search_subs_and_save_matches():
     else:
         excl = []
     regex_list = [
-        compile_regex(pattern)
-        for pattern in listio.read_list(args.patterns)
+        compile_regex(pattern) for pattern in listio.read_list(args.patterns)
     ]
     paths = iter_subs_files(args.inputdir)
     subs = read_subs(paths)
     matches = search_subs(subs, excl, regex_list)
     matches_list = (
         common.convert_match_to_list(match)
-        for match in matches
+        for match in itertools.chain(excl, matches)
     )
 
     listio.write_map(args.outputfile, matches_list)
